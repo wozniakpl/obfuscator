225084469fa3f58250161bb149c7e0da27289c8c
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-10-31 19:01:43 +0100

    works

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,101 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let obfuscateCommand = vscode.commands.registerCommand('obfuscate.start', async () => {
+        processText(context);
+    });
+    let resetCommand = vscode.commands.registerCommand('obfuscate.reset', async () => {
+        resetPreferences(context);
+    });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "obfuscator" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('obfuscator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from obfuscator!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(obfuscateCommand);
+    context.subscriptions.push(resetCommand);
 }
 
-// This method is called when your extension is deactivated
+async function resetPreferences(context: vscode.ExtensionContext) {
+    await context.globalState.update('caseSensitive', '');
+    await context.globalState.update('userInput', '');
+    await context.globalState.update('commandHistory', []);
+    vscode.window.showInformationMessage('Obfuscate settings have been reset.');
+}
+
+async function processText(context: vscode.ExtensionContext) {
+    const activeEditor = vscode.window.activeTextEditor;
+
+    if (activeEditor) {
+        let caseSensitive = context.globalState.get<string>('caseSensitive', '');
+        let commandHistory: string[] = context.globalState.get('commandHistory', []);
+
+        if (caseSensitive === '') {
+            const caseSensitiveOption = await vscode.window.showQuickPick(['Case Sensitive', 'Case Insensitive'], {
+                placeHolder: 'Do you want the search to be case sensitive?',
+            });
+
+            if (caseSensitiveOption === 'Case Insensitive') {
+                caseSensitive = 'false';
+            } else {
+                caseSensitive = 'true';
+            }
+
+            await context.globalState.update('caseSensitive', caseSensitive);
+        }
+
+        const selection = activeEditor.selection;
+        const highlightedText = activeEditor.document.getText(selection);
+
+        const quickPick = vscode.window.createQuickPick();
+        quickPick.items = commandHistory.map(label => ({ label }));
+        quickPick.matchOnDetail = true;
+        quickPick.placeholder = 'Select a previous command or type a new one';
+
+        quickPick.onDidAccept(async () => {
+            const userInput = quickPick.selectedItems.length > 0 ? quickPick.selectedItems[0].label : quickPick.value;
+            if (userInput !== undefined) {
+                processReplacementsAndHistory(userInput, highlightedText, activeEditor, selection, caseSensitive, context, commandHistory);
+            }
+            quickPick.dispose();
+        });
+
+        quickPick.show();
+    }
+}
+
+
+async function processReplacementsAndHistory(userInput: string, highlightedText: string, activeEditor: vscode.TextEditor, selection: vscode.Selection, caseSensitive: string, context: vscode.ExtensionContext, commandHistory: string[]) {
+    await context.globalState.update('userInput', userInput);
+    commandHistory = commandHistory.filter(cmd => cmd !== userInput);
+    commandHistory.unshift(userInput);
+    await context.globalState.update('commandHistory', commandHistory);
+
+    if (userInput !== '') {
+        const replacements = parseReplacements(userInput);
+        let newText = highlightedText;
+
+        for (const [word, newWord] of Object.entries(replacements)) {
+            const regex = new RegExp(word, caseSensitive === 'true' ? 'g' : 'gi');
+            newText = newText.replace(regex, newWord);
+        }
+
+        await activeEditor.edit((editBuilder) => {
+            editBuilder.replace(selection, newText);
+        });
+
+        processText(context);
+    }
+}
+
+function parseReplacements(input: string): { [key: string]: string } {
+    const pairs = input.split(',').map(s => s.trim());
+    const replacements: { [key: string]: string } = {};
+
+    for (const pair of pairs) {
+        const [word, newWord] = pair.split(':').map(s => s.trim());
+        if (word && newWord) {
+            replacements[word] = newWord;
+        }
+    }
+
+    return replacements;
+}
+
 export function deactivate() {}
